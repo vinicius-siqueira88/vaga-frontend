adabb8fa089e5d3e7b31a893d8cc72d43ef26efd
Marcos Vinicius de Siqueira <vinicius.siqueira88@gmail.com>
2015-03-19 23:11:00 -0300

    layout_Galeria 1.2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+function abreFechaMenu(){
+
+	var menu = document.getElementById("menuMobile");
+
+	if(menu.className == "menuMobilehide"){
+		
+		menu.className = "menuMobile";
+	}else{
+		
+		menu.className = "menuMobilehide";
+	}
+
+}
